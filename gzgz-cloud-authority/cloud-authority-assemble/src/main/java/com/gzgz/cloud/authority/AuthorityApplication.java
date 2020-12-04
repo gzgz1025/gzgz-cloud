5029b54e76e3455f7f2479881cf75500e36c48c2
gzgz <pzl688pzl>
2020-12-04 16:52:58 +0800

    增加swagger、增加mybatis代码生成器

--- gzgz-cloud-authority/cloud-authority-assemble/src/main/java/com/gzgz/cloud/authority/AuthorityApplication.java
+++ gzgz-cloud-authority/cloud-authority-assemble/src/main/java/com/gzgz/cloud/authority/AuthorityApplication.java
@@ -1,5 +1,6 @@
 package com.gzgz.cloud.authority;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Version: 1.0
  */
 @SpringBootApplication
+@MapperScan("com.gzgz.cloud.authority.dal")
 public class AuthorityApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthorityApplication.class,args);
--- gzgz-cloud-authority/cloud-authority-assemble/src/main/java/com/gzgz/cloud/authority/config/SwaggerConfiguration.java
+++ gzgz-cloud-authority/cloud-authority-assemble/src/main/java/com/gzgz/cloud/authority/config/SwaggerConfiguration.java
@@ -1,7 +1,14 @@
 package com.gzgz.cloud.authority.config;
 
 import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 /**
  * @ClassName: SwaggerConfiguration
@@ -13,4 +20,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableKnife4j
 public class SwaggerConfiguration {
+    @Bean(value = "defaultApi2")
+    public Docket defaultApi2() {
+        Docket docket=new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(new ApiInfoBuilder()
+                        .title("GZGZ-CLOUD API文档")
+                        .description("# swagger-bootstrap-ui-demo RESTful APIs")
+                        .termsOfServiceUrl("http://8.131.76.129:8010")
+                        .contact(new Contact("gzgz","xx@qq.com","gzgz1025@aliyun.com"))
+                        .version("1.0.0")
+                        .build())
+                //分组名称
+                .groupName("1.0.0版本")
+                .select()
+                //这里指定Controller扫描包路径
+                .apis(RequestHandlerSelectors.basePackage("com.zh.nb.controller"))
+                .paths(PathSelectors.any())
+                .build();
+        return docket;
+    }
 }
