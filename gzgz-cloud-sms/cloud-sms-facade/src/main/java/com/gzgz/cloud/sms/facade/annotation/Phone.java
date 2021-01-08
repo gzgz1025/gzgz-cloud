9e657501945bb57c179a96249a2078eb1ca6d824
gzgz <pzl688pzl>
2021-01-08 17:56:02 +0800

    正则表达式更改

--- gzgz-cloud-sms/cloud-sms-facade/src/main/java/com/gzgz/cloud/sms/facade/annotation/Phone.java
+++ gzgz-cloud-sms/cloud-sms-facade/src/main/java/com/gzgz/cloud/sms/facade/annotation/Phone.java
@@ -7,7 +7,7 @@ import org.hibernate.validator.constraints.Length;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import javax.validation.ReportAsSingleViolation;
-import javax.validation.constraints.Null;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
@@ -26,8 +26,8 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @ConstraintComposition(CompositionType.OR)
 @Pattern(regexp = "^1[3|4|5|7|8|9][0-9]{9}$")
-@Null
-@Length(min = 0, max = 0)
+@NotNull
+@Length(min = 11, max = 11)
 @Documented
 @Constraint(validatedBy = {})
 @Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE})
