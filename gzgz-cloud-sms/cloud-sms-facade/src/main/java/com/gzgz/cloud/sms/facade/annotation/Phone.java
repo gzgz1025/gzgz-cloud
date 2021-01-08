83172e9e4082f1934f2f7adf81327f3348941959
gzgz <pzl688pzl>
2021-01-08 18:04:22 +0800

    注解修改

--- gzgz-cloud-sms/cloud-sms-facade/src/main/java/com/gzgz/cloud/sms/facade/annotation/Phone.java
+++ gzgz-cloud-sms/cloud-sms-facade/src/main/java/com/gzgz/cloud/sms/facade/annotation/Phone.java
@@ -28,7 +28,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @ConstraintComposition(CompositionType.OR)
 @Pattern(regexp = "^1[3|4|5|7|8|9][0-9]{9}$")
 @Null
-@Length(min = 11, max = 11)
+@Length(min = 0, max = 0)
 @Documented
 @Constraint(validatedBy = {})
 @Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE})
